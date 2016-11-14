f91e6a6db0e4b949cbb1ca963f62b850f5f5a50b
SergeyAksenov <saxenov95@gmail.com>
2016-11-14 17:58:00 +0300

    the first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+__author__ = 'Sergey'
+
+from some import some_class
+
+
+smc = some_class(300)
---
+++ some.py
@@ -0,0 +1,9 @@
+__author__ = 'Sergey'
+
+
+class some_class:
+
+    def __init__(self, some__int):
+        print some__int
+
+
