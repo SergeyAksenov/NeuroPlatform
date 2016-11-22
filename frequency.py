5e54b3c4c80b45472e2add65c98040f8fda54faf
Sergey A. Aksenov <saxenov95@gmail.com>
2016-11-22 17:16:14 +0300

    word-context processing

--- frequency.py
+++ frequency.py
@@ -1,6 +1,6 @@
 import textmining
 import numpy as np
-
+from nltk import ngrams
 
 
 ########################################################################################################################
@@ -22,19 +22,47 @@ def create_frequency_matrix(documents, cutoff=2, path_to_save=None):
 ########################################################################################################################
 
 
-class base_frequency_matrix_creator_factory:
+class base_frequency_processor_factory:
 
-    def __init__(self, n_gram = 1, language='English'):
-        self.n_gram = n_gram
+    def __init__(self, language='English'):
         self.language = 'English'
 
-    def get_frequency_matrix_creator(self):
-        return
+    def get_frequency_matrix_creator(self, context_size=3):
+        return base_frequency_processor(self.language, context_size=context_size)
 
 
 ########################################################################################################################
 
 
-class base_frequency_matrix_creator:
+class base_frequency_processor:
 
-    
+    def __init__(self, context_size=3, language='English'):
+        self.context_size = context_size
+
+    def process(self, document, input_delimiter=' ', \
+                output_inner_delimiter=' ', output_outer_delimiter='\t'):
+        words = document.split(input_delimiter)
+        d  = {}
+        for i in range(self.context_size, len(words)):
+            context_words = words[i - self.context_size:i]
+            for w in context_words:
+                for u in context_words:
+                    # if not w in d:
+                    #     d[w] = {u : 1}
+                    # else:
+                    #     if u not in d[w]:
+                    #         d[w][u] = 1
+                    #     else:
+                    #         d[w][u] = d[w][u] + 1
+                    if not w+'__'+u in d:
+                        d[w+'__'+u] = 0
+                    else:
+                        d[w+'__'+u] = d[w+'__'+u] + 1
+        ret = []
+        for k in d.keys():
+            ret.append(k + output_inner_delimiter + str(d[k]))
+        return output_outer_delimiter.join(ret)
+
+
+########################################################################################################################
+
